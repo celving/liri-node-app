6dced22e075495ffcf2f296918561e312318d798
Colin Elving <viewtifuljho@hotmail.com>
2019-08-07 18:38:06 -0500

    concert-this command complete

--- liri.js
+++ liri.js
@@ -2,4 +2,26 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+
+var moment = require("moment");
+
+
+var command = process.argv[2];
+
+var parameter = process.argv.slice(3).join(" ");
+
+switch (command){
+    case "concert-this":
+    axios.get("https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp").then(
+        function(response) {
+            for (var i = 0; i < 5; i++){
+                console.log("Venue: " + response.data[i].venue.name +
+                "\nLocation: " + response.data[i].venue.city + " " + response.data[i].venue.region +
+                "\nDate: " + moment(response.data[i].datetime).format("MM/DD/YYYY") +
+                "\n----------------")
+            }
+        }
+        
+    );
+}
