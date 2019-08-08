48fcc6835edd6ee9b9183b546dc6974d387e0a04
Colin Elving <viewtifuljho@hotmail.com>
2019-08-08 13:06:00 -0500

    movie-this complete

--- liri.js
+++ liri.js
@@ -33,18 +33,70 @@ switch (command) {
             };
         });
     case "spotify-this-song":
+        if (!parameter){
+            parameter = "The Sign";
+        };
+        
+        
         spotify.search({ type: "track", query: parameter }, function(err, data) {
             if (err) {
                 return console.log("Error occurred: " + err)
             }
-            var firstTrack = data.tracks.items[0];
+            var firstTrack;
+            
+            if (parameter === "The Sign"){
+                firstTrack = data.tracks.items[2];
+            }
+
+            else {
+                firstTrack = data.tracks.items[0];
+            }
+            var preview = "";
+
+            if (firstTrack.preview_url === null){
+                preview = "Unavailable"
+            }
+
+            else {
+                preview = firstTrack.preview_url;
+            };
 
             console.log(firstTrack.name +
             "\nArtist: " + firstTrack.artists[0].name +
-            "\nPreview: " + firstTrack.preview_url +
+            "\nPreview: " + preview +
             "\nAlbum: " + data.tracks.items[0].album.name +
             "\n----------------");
         });
+    case "movie-this":
+        if (!parameter) {
+            parameter = "Mr. Nobody";
+        };
+            axios.get("http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=trilogy").then(
+                function(response) {
+                  console.log(response.data.Title +
+                    "\nReleased: " + response.data.Year +
+                    "\niMDB Rating: " + response.data.imdbRating +
+                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                    "\nProduced in " + response.data.Country +
+                    "\nLanguage: " + response.data.Language +
+                    "\nActors: " + response.data.Actors +
+                    "\n" + response.data.Plot);
+                })
+                .catch(function(error) {
+                  if (error.response) {
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                  } else if (error.request) {
+                    console.log(error.request);
+                  } else {
+                    console.log("Error", error.message);
+                  }
+                  console.log(error.config);
+                });
 
 
-}
+};
